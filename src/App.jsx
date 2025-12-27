dbab164796fd6a8038bd405833025542969d953b
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2025-12-26 19:21:50 -0600

    feat!: template for a site under construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Header } from "./components/Header/Header";
-import { Main } from "./components/Main/Main";
+// import { Header } from "./components/Header/Header";
+// import { Main } from "./components/Main/Main";
 import { useEffect } from "react";
 
 function App() {
@@ -12,8 +12,13 @@ function App() {
     <>
       <div id="particles-js" className="w-screen h-screen fixed top-0 -z-10"></div>
       <div className="max-w-[1600px] mx-auto">
-        <Header />
-        <Main />
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-4xl text-center font-bold mb-4 text-[#0096ff]">Sitio en construcción</h1>
+          <p className="text-lg text-center">¡Próximamente una nueva versión esta por llegar!</p>
+          <p className="text-lg text-center font-semibold">contacto@yovanymorales.com</p>
+        </div>
+        {/* <Header className="hidden" /> */}
+        {/* <Main /> */}
       </div>
     </>
   )
